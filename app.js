b78910e2d96525cd6f676127594d66dd0bab7101
chords2003 <chords2003@yahoo.com>
2024-02-09 01:05:11 -0500

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('How are you world'));
+app.get('/', (req, res) => res.send('How are you world, A new pipeline is created with CI and CD!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
